7bb5e6aac0fd405e1f72fdc3b8a3365a465acb61
Sabbir Ahmed <sabbir15-4993@diu.edu.bd>
2020-10-03 14:35:58 +0600

    Initial commit

---
+++ resources/views/admin/department/create.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.layouts.master')
+
+@section('content')
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                @if(Session::has('message'))
+                    <div class="alert alert-success">
+                        {{Session::get('message')}}
+                    </div>
+                @endif
+
+                <form action="{{route('department.store')}}" method="post">
+                    @csrf
+                    <div class="card">
+                        <div class="card-header">{{ __('Create Department') }}</div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Description</label>
+                                <textarea name="description" id="" cols="30" rows="5" class="form-control @error('description') is-invalid @enderror"></textarea>
+                                @error('description')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/admin/department/index.blade.php
@@ -0,0 +1,80 @@
+@extends('admin.layouts.master')
+
+@section('content')
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                @if(Session::has('message'))
+                    <div class="alert alert-success">
+                        {{Session::get('message')}}
+                    </div>
+                @endif
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">All Departments</li>
+                    </ol>
+                </nav>
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>SN</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @if(
+                        count($departments)>0
+                        )
+
+                        @foreach($departments as $key => $department)
+                            <tr>
+                                <td>{{$key+1}}</td>
+                                <td>{{$department->name}}</td>
+                                <td>{{$department->description}}</td>
+                                <td><a href="{{route('department.edit',[$department->id])}}"><i class="fas fa-edit"></i></a></td>
+                                <td><a href="#" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-trash"></i></a>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <form action="{{route('department.destroy',[$department->id])}}" method="post">
+                                            @csrf
+                                            {{method_field('DELETE')}}
+
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel">Delete Confirm!!</h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Do you Want to Delete ?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <!-- Modal End -->
+                                </td>
+                            </tr>
+                        @endforeach
+                        @else
+                             <td>No Department to Display</td>
+                    @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
