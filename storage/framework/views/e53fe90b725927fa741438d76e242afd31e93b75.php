60972296000ec0d1559692e41bbda2ffd076e68c
Sabbir Ahmed <sabbir15-4993@diu.edu.bd>
2020-10-14 17:58:57 +0600

    Dashboard Change

--- storage/framework/views/e53fe90b725927fa741438d76e242afd31e93b75.php
+++ storage/framework/views/e53fe90b725927fa741438d76e242afd31e93b75.php
@@ -1,25 +1,29 @@
 <div id="layoutSidenav_content">
     <main>
         <div class="container-fluid">
-            <h1 class="mt-4">Dashboard</h1>
-            <ol class="breadcrumb mb-4">
-                <li class="breadcrumb-item active">Dashboard</li>
-            </ol>
+
+            <div class="mt-3">
+                <ol class="breadcrumb mb-4">
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </div>
             <div class="row">
                 <div class="col-xl-3 col-md-6">
                     <div class="card bg-primary text-white mb-4">
-                        <div class="card-body">Primary Card</div>
+                        <div class="card-body">Employees</div>
+                        <p><i class="fas fa-user fa-fw" style="font-size: 100px"></i></p>
                         <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link" href="#">View Details</a>
+                            <a class="small text-white stretched-link" href="<?php echo e(route('users.index')); ?>" style="font-size: 18px"><?php echo e(App\User::all()->count()); ?></a>
                             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-3 col-md-6">
                     <div class="card bg-warning text-white mb-4">
-                        <div class="card-body">Warning Card</div>
+                        <div class="card-body">Departments</div>
+                        <p><i class="fas fa-home fa-fw" style="font-size: 100px"></i></p>
                         <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link" href="#">View Details</a>
+                            <a class="small text-white stretched-link" href="<?php echo e(route('department.index')); ?>" style="font-size: 18px"><?php echo e(App\Department::all()->count()); ?></a>
                             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                         </div>
                     </div>
@@ -43,6 +47,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="row">
                 <div class="col-xl-6">
                     <div class="card mb-4">
@@ -63,6 +68,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="card mb-4">
                 <div class="card-header">
                     <i class="fas fa-table mr-1"></i>
@@ -100,6 +106,10 @@
                                     <td>2011/04/25</td>
                                     <td>$320,800</td>
                                 </tr>
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
                             </tbody>
                         </table>
                     </div>
